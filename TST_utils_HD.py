1bb1b00e88654e7dde5d391ffc03a9e3a741bd1d
alexwolfliuf <alexwolf.liuf@gmail.com>
2019-10-08 18:58:21 +0100

    update utils

--- TST_utils_HD.py
+++ TST_utils_HD.py
@@ -25,13 +25,16 @@ class ModelLatentF(torch.nn.Module):
 
         self.latent = torch.nn.Sequential(
             torch.nn.Linear(x_in, H, bias=True),
+            # torch.nn.BatchNorm1d(H),
             torch.nn.Softplus(),
             torch.nn.Linear(H, H, bias=True),  # +1 for high test power
+            # torch.nn.BatchNorm1d(H),
             torch.nn.Softplus(),
             torch.nn.Linear(H, H, bias=True),
+            # torch.nn.BatchNorm1d(H),
             torch.nn.Softplus(),
             # torch.nn.Linear(H, H, bias=True),
-            # torch.nn.ReLU(),
+            # torch.nn.Softplus(),
             torch.nn.Linear(H, x_out, bias=True),
         )
 
@@ -120,7 +123,7 @@ def h1_mean_var_gram(Kx, Ky, Kxy, is_var_computed, use_1sample_U=True):
         mmd2 = xx - 2 * xy + yy
 
     if not is_var_computed:
-        return mmd2, None
+        return mmd2, None, Kxyxy
 
     hh = Kx+Ky-Kxy-Kxy.transpose(0,1)
     V1 = torch.dot(hh.sum(1)/ny,hh.sum(1)/ny) / ny
@@ -128,7 +131,7 @@ def h1_mean_var_gram(Kx, Ky, Kxy, is_var_computed, use_1sample_U=True):
     V2 = (hh).sum() / (nx) / nx
     varEst = 4*(V1 - V2**2)
     if  varEst == 0.0:
-        print('error!!'+str(V1))
+        print('error_var!!'+str(V1))
     ## unbiased var
     # hh = hh - torch.diag(torch.diag(hh))
     # # V1_t = torch.einsum('ij,in->ijn',[hh,hh])
@@ -160,7 +163,7 @@ def MMDu(Fea, len_s, Fea_org, sigma, sigma0=0.1, is_smooth=True, is_mixture=Fals
     Y = Fea[len_s:, :]
     X_org = Fea_org[0:len_s, :]
     Y_org = Fea_org[len_s:, :]
-    epsilon = 10 ** (-10)
+    epsilon = 10**(-10)
     L = 1
 
     nx = X.shape[0]
@@ -171,6 +174,9 @@ def MMDu(Fea, len_s, Fea_org, sigma, sigma0=0.1, is_smooth=True, is_mixture=Fals
     Dxx_org = Pdist2(X_org, X_org)
     Dyy_org = Pdist2(Y_org, Y_org)
     Dxy_org = Pdist2(X_org, Y_org)
+    K_Ix = torch.eye(nx).cuda()
+    K_Iy = torch.eye(ny).cuda()
+    # sigma1 = Dxy.view(-1, 1).squeeze().kthvalue(int(Dxy.size(0) * Dxy.size(1) * 0.01))[0]
     if is_mixture:
         if is_smooth:
             Kx = torch.exp(-Dxx / sigma0 - Dxx_org / sigma)
@@ -182,41 +188,17 @@ def MMDu(Fea, len_s, Fea_org, sigma, sigma0=0.1, is_smooth=True, is_mixture=Fals
             Kxy = torch.exp(-Dxy / sigma0)
     else:
         if is_smooth:
+            Kx = (1-epsilon) * torch.exp(-(Dxx / sigma0)**L -Dxx_org / sigma) + epsilon * torch.exp(-Dxx_org / sigma)
+            Ky = (1-epsilon) * torch.exp(-(Dyy / sigma0)**L -Dyy_org / sigma) + epsilon * torch.exp(-Dyy_org / sigma)
+            Kxy = (1-epsilon) * torch.exp(-(Dxy / sigma0)**L -Dxy_org / sigma) + epsilon * torch.exp(-Dxy_org / sigma)
 
-            Kx = (1-epsilon) * torch.exp(-(Dxx / sigma0) - (Dxx_org / sigma))**L + epsilon * torch.exp(-Dxx_org / sigma)
-            Ky = (1-epsilon) * torch.exp(-(Dyy / sigma0) - (Dyy_org / sigma))**L + epsilon * torch.exp(-Dyy_org / sigma)
-            Kxy = (1-epsilon) * torch.exp(-(Dxy / sigma0) - (Dxy_org / sigma))**L + epsilon * torch.exp(-Dxy_org / sigma)
+            # Kx = (1 - epsilon) * torch.exp(-(Dxx / sigma0) ** L ) + epsilon * K_Ix
+            # Ky = (1 - epsilon) * torch.exp(-(Dyy / sigma0) ** L ) + epsilon * K_Iy
+            # Kxy = (1 - epsilon) * torch.exp(-(Dxy / sigma0) ** L )
         else:
             Kx = torch.exp(-Dxx / sigma0)
             Ky = torch.exp(-Dyy / sigma0)
             Kxy = torch.exp(-Dxy / sigma0)
-
-    return h1_mean_var_gram(Kx, Ky, Kxy, is_var_computed, use_1sample_U)
-
-def MMDu_linear_kernel(Fea, len_s, is_var_computed=True, use_1sample_U=True):
-    """
-    X: nxd numpy array
-    Y: nxd numpy array
-    k: a Kernel object
-    is_var_computed: if True, compute the variance. If False, return None.
-    use_1sample_U: if True, use one-sample U statistic for the cross term
-      i.e., k(X, Y).
-
-    Code based on Arthur Gretton's Matlab implementation for
-    Bounliphone et. al., 2016.
-
-    return (MMD^2, var[MMD^2]) under H1
-    """
-    try:
-        X = Fea[0:len_s, :]
-        Y = Fea[len_s:, :]
-    except:
-        X = Fea[0:len_s].unsqueeze(1)
-        Y = Fea[len_s:].unsqueeze(1)
-
-    Kx = X.mm(X.transpose(0,1))
-    Ky = Y.mm(Y.transpose(0,1))
-    Kxy = X.mm(Y.transpose(0,1))
 
     return h1_mean_var_gram(Kx, Ky, Kxy, is_var_computed, use_1sample_U)
 
@@ -292,6 +274,45 @@ def TST_MMD_adaptive_bandwidth(Fea, N_per, N1, Fea_org, sigma, sigma0, alpha, de
         threshold = S_mmd_vector[np.int(np.ceil(N_per * (1 - alpha)))]
     return h, threshold, mmd_value.item()
 
+def TST_MMD_ab_difference_size(Fea, N_per, N1, Fea_org, sigma, sigma0, alpha, device, dtype):
+    mmd_vector = np.zeros(N_per)
+    # mmd_value = MyMMD(Fea, LM, N1).detach().numpy()
+    TEMP = MMDu(Fea, N1, Fea_org, sigma, sigma0, is_smooth=False,is_var_computed=False)
+    mmd_value = get_item(TEMP[0],is_cuda)
+    Kxyxy = TEMP[2]
+    count = 0
+    # Fea = get_item(Fea,is_cuda)
+    nxy = Fea.shape[0]
+    nx = N1
+
+    for r in range(N_per):
+        # print r
+        ind = np.random.choice(nxy, nxy, replace=False)
+        # divide into new X, Y
+        indx = ind[:nx]
+        # print(indx)
+        indy = ind[nx:]
+        Kx = Kxyxy[np.ix_(indx, indx)]
+        # print(Kx)
+        Ky = Kxyxy[np.ix_(indy, indy)]
+        Kxy = Kxyxy[np.ix_(indx, indy)]
+
+        TEMP = h1_mean_var_gram(Kx, Ky, Kxy, is_var_computed=False)
+        mmd_vector[r] = TEMP[0]
+        if mmd_vector[r] > mmd_value:
+            count = count + 1
+        if count > np.ceil(N_per * alpha):
+            h = 0
+            threshold = "NaN"
+            break
+        else:
+            h = 1
+    if h == 1:
+        S_mmd_vector = np.sort(mmd_vector)
+        #        print(np.int(np.ceil(N_per*alpha)))
+        threshold = S_mmd_vector[np.int(np.ceil(N_per * (1 - alpha)))]
+    return h, threshold, mmd_value.item()
+
 def TST_MMD_u(Fea, N_per, N1, Fea_org, sigma, sigma0, alpha,  device, dtype):
     mmd_vector = np.zeros(N_per)
     TEMP = MMDu(Fea, N1, Fea_org, sigma, sigma0)
@@ -330,9 +351,9 @@ def TST_MMD_u(Fea, N_per, N1, Fea_org, sigma, sigma0, alpha,  device, dtype):
         threshold = S_mmd_vector[np.int(np.ceil(N_per * (1 - alpha)))]
     return h, threshold, mmd_value.item()
 
-def TST_MMD_u_linear_kernel(Fea, N_per, N1, alpha,  device, dtype):
+def TST_MMD_u_different_size(Fea, N_per, N1, Fea_org, sigma, sigma0, alpha,  device, dtype):
     mmd_vector = np.zeros(N_per)
-    TEMP = MMDu_linear_kernel(Fea, N1)
+    TEMP = MMDu(Fea, N1, Fea_org, sigma, sigma0,is_var_computed=False)
     mmd_value = get_item(TEMP[0], is_cuda)
     Kxyxy = TEMP[2]
     count = 0
@@ -432,19 +453,19 @@ def TST_C2ST(S,N1,N_per,alpha,model_C2ST, w_C2ST, b_C2ST,device,dtype):
     h = 0
     if STAT.item() > threshold:
         h = 1
-    if STAT.item() < threshold_lower:
-        h = 1
+    # if STAT.item() < threshold_lower:
+    #     h = 1
     return h, threshold, STAT
 
-def TST_LCE(S,N1,N_per,alpha,model_C2ST, w_C2ST, b_C2ST,device,dtype):
+def TST_C2ST_D(S,N1,N_per,alpha,discriminator,device,dtype):
     np.random.seed(seed=1102)
     torch.manual_seed(1102)
     torch.cuda.manual_seed(1102)
     N = S.shape[0]
     f = torch.nn.Softmax()
-    output = f(model_C2ST(S).mm(w_C2ST) + b_C2ST)
-    # pred_C2ST = output.max(1, keepdim=True)[1]
-    STAT = abs(output[:N1,0].type(torch.FloatTensor).mean() - output[N1:,0].type(torch.FloatTensor).mean())
+    output = discriminator(S)
+    pred_C2ST = output.max(1, keepdim=True)[1]
+    STAT = abs(pred_C2ST[:N1].type(torch.FloatTensor).mean() - pred_C2ST[N1:].type(torch.FloatTensor).mean())
     STAT_vector = np.zeros(N_per)
     for r in range(N_per):
         ind = np.random.choice(N, N, replace=False)
@@ -452,15 +473,15 @@ def TST_LCE(S,N1,N_per,alpha,model_C2ST, w_C2ST, b_C2ST,device,dtype):
         ind_X = ind[:N1]
         ind_Y = ind[N1:]
         # print(indx)
-        STAT_vector[r] = abs(output[ind_X,0].type(torch.FloatTensor).mean() - output[ind_Y,0].type(torch.FloatTensor).mean())
+        STAT_vector[r] = abs(pred_C2ST[ind_X].type(torch.FloatTensor).mean() - pred_C2ST[ind_Y].type(torch.FloatTensor).mean())
     S_vector = np.sort(STAT_vector)
     threshold = S_vector[np.int(np.ceil(N_per * (1 - alpha)))]
     threshold_lower = S_vector[np.int(np.ceil(N_per *  alpha))]
     h = 0
     if STAT.item() > threshold:
         h = 1
-    if STAT.item() < threshold_lower:
-        h = 1
+    # if STAT.item() < threshold_lower:
+    #     h = 1
     return h, threshold, STAT
 
 def TST_MMD_b(Fea, N_per, LM, N1, alpha, device, dtype):
