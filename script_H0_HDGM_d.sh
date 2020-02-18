3656a73dbe6f44fb6d710fdf8639993bae79ab7f
fengliu90 <alexwolf.liuf@gmail.com>
2020-02-19 01:36:52 +1100

    Deleted files manually

--- script_H0_HDGM_d.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# This is our first script.
-CUDA_VISIBLE_DEVICES=3 python main_HDGM_H0.py --d 25
-CUDA_VISIBLE_DEVICES=3 python main_HDGM_H0.py --d 20
-CUDA_VISIBLE_DEVICES=3 python main_HDGM_H0.py --d 15
-CUDA_VISIBLE_DEVICES=3 python main_HDGM_H0.py --d 10
-CUDA_VISIBLE_DEVICES=3 python main_HDGM_H0.py --d 5
-CUDA_VISIBLE_DEVICES=3 python main_HDGM_H0.py --d 3
